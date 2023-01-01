569d5546e766018bf7a7615268320eb38fff6823
rajiv <rajiv@gmail.com>
2023-01-01 23:42:47 +0530

    added custom block plugin

---
+++ web/modules/custom/d4drupal_block/src/Plugin/Block/D4DrupalBlock.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\d4drupal_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+/*use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+*/
+
+/**
+ * Provides Simple block for D4drupal.
+ *
+ * @Block(
+ *  id = "d4drupal_block",
+ *  admin_label = @Translation("D4drupal block")
+ * )
+ */
+class D4DrupalBlock extends BlockBase {
+
+  /*protected $formBuilder;
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    $this->formBuilder = $form_builder;
+  }
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')// form_builder service will inject inside class
+    );
+  }
+  */
+
+  /**
+   * {#inheritdoc}
+   */
+  public function build() {
+     return[
+       "#markup" => "Welcome to d4drupal",
+     ];
+
+    /*$form = $this->formBuilder->getForm('Drupal\rk_customlogin\Form\RKCustomForm');
+    return $form;
+    */
+  }
+
+}
